ca0dccb1e62b4c292a3769f006452a393a96ccf9
suThyy <oqnvuz0704@gmail.com>
2026-06-23 01:44:23 +0700

    init

---
+++ src/main/java/com/oqnsuthyz/chatapp/ChatappApplication.java
@@ -0,0 +1,21 @@
+package com.oqnsuthyz.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ChatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String getGreeting() {
+		return "Hello, World!";
+	}
+
+}
